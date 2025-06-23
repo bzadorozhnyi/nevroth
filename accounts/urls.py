392a0a3386a1e3c56f02b049edeb9c2a0a5f4dc0
bzadorozhnyi <bohzador@gmail.com>
2025-06-23 11:09:42 +0300

    implemented basic registration flow;

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,10 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+from accounts.views import RegistrationView
+
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # Login
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # Refresh token
+    path('register/', RegistrationView.as_view(), name='register'),
 ]
