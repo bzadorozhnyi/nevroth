ac4a16bf081177ff8fa8410965668fe08631e392
bzadorozhnyi <bohzador@gmail.com>
2025-06-24 11:24:47 +0300

    PWN-6 added factories to support test setup for users and verify tokens;

---
+++ accounts/tests/factories/user.py
@@ -0,0 +1,38 @@
+import uuid
+
+import factory
+from django.contrib.auth.hashers import make_password
+from factory.django import DjangoModelFactory
+
+from accounts.models import User
+from faker import Faker
+
+faker = Faker()
+
+
+class BaseUserFactory(DjangoModelFactory):
+    class Meta:
+        model = User
+
+    email = factory.LazyAttribute(lambda o: f"{faker.first_name().lower()}.{uuid.uuid4().hex
+    [:4]}@example.com")
+    full_name = factory.Faker("name")
+    password = factory.PostGenerationMethodCall("set_password", "password")
+
+    is_superuser = False
+    is_staff = False
+
+    @factory.post_generation
+    def set_password(self, created, extracted, **kwargs):
+        if extracted:
+            self.password = make_password(extracted)
+            if created:
+                self.save()
+
+
+class AdminFactory(BaseUserFactory):
+    role = User.Role.ADMIN
+
+
+class MemberFactory(BaseUserFactory):
+    role = User.Role.MEMBER
