2edcc90ebb95fd4135a3f297ee77183a79f5c590
bzadorozhnyi <bohzador@gmail.com>
2025-06-25 15:35:42 +0300

    PWN-7 localized field labels in Habit model;

--- habits/models.py
+++ habits/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class Habit(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(max_length=255)
+    name = models.CharField(_("name"), max_length=100)
+    description = models.TextField(_("description"), max_length=255)
