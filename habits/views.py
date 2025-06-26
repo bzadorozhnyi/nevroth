70ee1735054a705c4bbd1d325be7db1756e80a6a
bzadorozhnyi <bohzador@gmail.com>
2025-06-26 11:38:45 +0300

    fix

--- habits/views.py
+++ habits/views.py
@@ -1,5 +1,4 @@
 from rest_framework import viewsets
-from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from habits.models import Habit
 from habits.permissions import RoleBasedHabitPermission
@@ -7,7 +6,6 @@ from habits.serializers import HabitSerializer
 
 
 class HabitViewSet(viewsets.ModelViewSet):
-    authentication_classes = [JWTAuthentication]
     queryset = Habit.objects.all()
     serializer_class = HabitSerializer
     permission_classes = [RoleBasedHabitPermission]
