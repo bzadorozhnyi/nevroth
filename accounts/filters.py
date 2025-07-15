c6814106556b1e02a08f6762b91a420c4ead24cb
Bohdan Zadorozhnyi <53794904+bzadorozhnyi@users.noreply.github.com>
2025-07-15 15:21:04 +0300

    PWN-19 changed UserFilter;
    Co-authored-by: Yura Bysaha <yurabysaha@gmail.com>

--- accounts/filters.py
+++ accounts/filters.py
@@ -6,8 +6,9 @@ User = get_user_model()
 
 
 class UserFilter(django_filters.FilterSet):
-    query = django_filters.CharFilter(
-        method="filter_query",
+        query = django_filters.CharFilter(
+        field_name="full_name",
+        lookup_expr="icontains",
         help_text=_("Search by full name"),
         label=_("Search Query"),
     )
