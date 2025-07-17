38f6d444e16fe755acd84f33faf810f30aaa8445
bzadorozhnyi <bohzador@gmail.com>
2025-07-17 11:21:00 +0300

    PWN-21 added admin panel for ChatMessage model;

--- chats/admin.py
+++ chats/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from chats.models import Chat, ChatMember
+from chats.models import Chat, ChatMember, ChatMessage
 
 
 @admin.register(Chat)
@@ -19,3 +19,16 @@ class ChatMemberAdmin(admin.ModelAdmin):
     list_filter = ("created_at",)
     autocomplete_fields = ("user", "chat")
     ordering = ("-created_at",)
+
+
+@admin.register(ChatMessage)
+class ChatMessageAdmin(admin.ModelAdmin):
+    list_display = ("id", "chat", "sender", "content_short", "created_at", "updated_at")
+    list_filter = ("chat", "sender", "created_at")
+    search_fields = ("content", "sender__full_name", "chat__id")
+    readonly_fields = ("created_at", "updated_at")
+
+    def content_short(self, obj):
+        return obj.content if len(obj.content) <= 50 else obj.content[:47] + "..."
+
+    content_short.short_description = "Content"
