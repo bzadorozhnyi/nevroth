ae15f06f4c77079784b8238b381b54870ae039ea
bzadorozhnyi <bohzador@gmail.com>
2025-07-17 15:49:01 +0300

    PWN-21 implemented list chat messages endpoint;

--- chats/urls.py
+++ chats/urls.py
@@ -2,7 +2,7 @@ from django.urls.conf import path, include
 
 from rest_framework.routers import DefaultRouter
 
-from chats.views import ChatListCreateView, ChatMessageView
+from chats.views import ChatListCreateView, ChatMessageView, ChatMessageListView
 
 router = DefaultRouter()
 router.register("messages", ChatMessageView, basename="chat-message")
@@ -12,6 +12,11 @@ urlpatterns = [
         "chats/",
         ChatListCreateView.as_view(),
         name="chat-list-create",
+    ),
+    path(
+        "chats/<int:id>/messages/",
+        ChatMessageListView.as_view(),
+        name="chat-messages-list",
     ),
     path("", include(router.urls)),
 ]
