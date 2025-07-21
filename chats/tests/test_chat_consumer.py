abf6686258c10491f93adf7d2b423569161cbb93
bzadorozhnyi <bohzador@gmail.com>
2025-07-21 15:54:27 +0300

    PWN-22 replaced URLRouter with application;

--- chats/tests/test_chat_consumer.py
+++ chats/tests/test_chat_consumer.py
@@ -1,19 +1,14 @@
-from channels.routing import URLRouter
-from chats.consumers import ChatConsumer
 from channels.testing import WebsocketCommunicator
 
 from django.test.testcases import TestCase
-from django.urls import re_path
+
+from nevroth.asgi import application
 
 
 class ChatConsumerTests(TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.application = URLRouter(
-            [
-                re_path(r"ws/chats/(?P<chat_id>\d+)/$", ChatConsumer.as_asgi()),
-            ]
-        )
+        cls.application = application
 
     async def test_websocket_connect_and_disconnect(self):
         communicator = WebsocketCommunicator(
